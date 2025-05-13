8ca5493ef6ad3bdecbe61a221d91a08a0e4069d3
Nico <nicoalesi.cs@gmail.com>
2025-05-13 18:28:43 +0200

    new: add custom exception
    Add a custom exception to notify that
    Conclave's setup has gone wrong.

---
+++ src/ConclaveSetupException.java
@@ -0,0 +1,9 @@
+public class ConclaveSetupException extends Exception {
+    public ConclaveSetupException() {
+        super();
+    }
+
+    public ConclaveSetupException(String message) {
+        super(message);
+    }
+}
